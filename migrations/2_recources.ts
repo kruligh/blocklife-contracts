acee08ab667f85ca31693264c07c543db40ffbba
Dominik Kroliczek <dominik@kroliczek.com>
2018-03-06 20:41:42 +0100

    Add migarion, Installing with yarn
    Npm install hangs and sucks

---
+++ migrations/2_recources.ts
@@ -0,0 +1,19 @@
+import {ProjectArtifacts} from 'project';
+import {Deployer} from 'truffle';
+
+declare const artifacts: ProjectArtifacts;
+
+const Resource = artifacts.require('./Resource.sol');
+
+async function deploy(deployer: Deployer) {
+  const resName = 'Gold';
+  const resCode = 'GLDXD';
+  const resDecimals = 18;
+  await deployer.deploy(Resource, resName, resCode, resDecimals);
+}
+
+function migrate(deployer: Deployer) {
+  deployer.then(() => deploy(deployer));
+}
+
+export = migrate;
